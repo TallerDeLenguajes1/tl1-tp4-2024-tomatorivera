4c90edbebd4dba00d11900f154006dae9449325e
tomatorivera <tomatorivera@gmail.com>
2024-05-05 23:00:33 -0300

    Hice el punto 2 y agregué una nueva función para eliminar nodos de una lista

--- tp4.c
+++ tp4.c
@@ -20,21 +20,23 @@ typedef struct Nodo
 } Nodo;
 
 // Funciones
-Nodo * crearListaVacia();
+Nodo *crearListaVacia();
 Tarea crearTarea();
-void insertarNodo(Nodo ** start, Nodo * nuevaTarea);
-Nodo * crearNodo(Tarea tarea);
-void mostrarLista(Nodo * start);
-void liberarMemoria(Nodo ** start);
+void insertarNodo(Nodo **start, Nodo *nuevaTarea);
+Nodo *crearNodo(Tarea tarea);
+void eliminarNodo(Nodo **start);
+void mostrarLista(Nodo *start);
+void liberarMemoria(Nodo **start);
+Tarea seleccionarTarea(Nodo **start);
 
-int main() 
+int main()
 {
-    Nodo * tareasPendientes = crearListaVacia();
-    Nodo * tareasRealizadas = crearListaVacia();
+    Nodo *tareasPendientes = crearListaVacia();
+    Nodo *tareasRealizadas = crearListaVacia();
 
     int opcion = 0;
 
-    // Menú de opciones para la carga de tareas
+    // PUNTO 1 - Menú de opciones para la carga de tareas pendientes
     do
     {
         printf("\n*** CARGA DE TAREAS ***");
@@ -45,29 +47,65 @@ int main()
 
         switch (opcion)
         {
-            case 1:
-                insertarNodo(&tareasPendientes, crearNodo(crearTarea()));
-                break;
-            case 2:
-                printf("\n*** CARGA DE TAREAS FINALIZADA ***\n");
-                break;
-            default:
-                printf("\n[!] Ingrese una opcion valida\n");
-                break;
+        case 1:
+            insertarNodo(&tareasPendientes, crearNodo(crearTarea()));
+            break;
+        case 2:
+            printf("\n*** CARGA DE TAREAS FINALIZADA ***\n");
+            break;
+        default:
+            printf("\n[!] Ingrese una opcion valida\n");
+            break;
         }
 
     } while (opcion != 2);
 
-    mostrarLista(tareasPendientes);
-    
+    // PUNTO 2 - Menú para transferir tareas pendientes a realizadas
+    do
+    {
+        printf("\n*** REALIZACION DE TAREAS ***");
+        printf("\n\t1. Marcar como pendiente una nueva tarea");
+        printf("\n\t2. Ver tareas pendientes");
+        printf("\n\t3. Ver tareas realizadas");
+        printf("\n\t4. Finalizar transferencia de tareas");
+        printf("\n> Digite su opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            printf(""); // C no me permite poner una declaración luego de un label unu
+            Tarea tareaTransferida = seleccionarTarea(&tareasPendientes);
+
+            // Si el ID de la tarea devuelta es distinto de -1, quiere decir que se seleccionó una tarea existente
+            if (tareaTransferida.TareaID != -1) insertarNodo(&tareasRealizadas, crearNodo(tareaTransferida));
+
+            break;
+        case 2:
+            mostrarLista(tareasPendientes);
+            break;
+        case 3:
+            mostrarLista(tareasRealizadas);
+            break;
+        case 4:
+            printf("\n*** TRANSFERENCIA DE TAREAS FINALIZADA ***\n");
+            break;
+        default:
+            printf("\n[!] Ingrese una opcion valida\n");
+            break;
+        }
+
+    } while (opcion != 4);
+
     // Libero memoria de las listas
     liberarMemoria(&tareasPendientes);
+    liberarMemoria(&tareasRealizadas);
 }
 
 /**
  * Crea una lista enlazada vacía
  */
-Nodo * crearListaVacia()
+Nodo *crearListaVacia()
 {
     return NULL;
 }
@@ -75,7 +113,8 @@ Nodo * crearListaVacia()
 /**
  * Solicita al usuario los datos necesarios para crear una nueva Tarea
  */
-Tarea crearTarea() {
+Tarea crearTarea()
+{
     printf("\n\n*** INSERTANDO UNA NUEVA TAREA ***");
 
     Tarea nuevaTarea;
@@ -87,7 +126,7 @@ Tarea crearTarea() {
     fflush(stdin);
     printf("\n\t- Ingrese la descripcion: ");
     gets(buff);
-    nuevaTarea.Descripcion = (char *)malloc( (strlen(buff) + 1) * sizeof(char) );
+    nuevaTarea.Descripcion = (char *)malloc((strlen(buff) + 1) * sizeof(char));
     strcpy(nuevaTarea.Descripcion, buff);
 
     // Ingreso la duración verificando que sea un número entre 10 y 100
@@ -98,21 +137,22 @@ Tarea crearTarea() {
         printf("\t- Ingrese la duracion: ");
         scanf("%d", &duracion);
 
-        if ((duracion < 10) || (duracion > 100)) printf("\n\t[!] Duracion invalida [!]\n\n");
-    } while ( (duracion < 10) || (duracion > 100) );
+        if ((duracion < 10) || (duracion > 100))
+            printf("\n\t[!] Duracion invalida [!]\n\n");
+    } while ((duracion < 10) || (duracion > 100));
 
     nuevaTarea.Duracion = duracion;
 
     return nuevaTarea;
-}   
+}
 
 /**
  * Inserta un nuevo nodo al final de una lista de tareas
- * 
+ *
  * @param start      Cabecera de la lista
  * @param nuevaTarea Nodo a insertar
  */
-void insertarNodo(Nodo ** start, Nodo * nuevaTarea)
+void insertarNodo(Nodo **start, Nodo *nuevaTarea)
 {
     if (*start == NULL)
     {
@@ -121,7 +161,7 @@ void insertarNodo(Nodo ** start, Nodo * nuevaTarea)
     }
     else
     {
-        Nodo * aux = *start;
+        Nodo *aux = *start;
         while (aux->Siguiente)
         {
             aux = aux->Siguiente;
@@ -132,14 +172,14 @@ void insertarNodo(Nodo ** start, Nodo * nuevaTarea)
 
 /**
  * Crea un nuevo nodo a partir de los datos de una estructura Tarea enviada por parámetro
- * 
+ *
  * @param tarea Datos de la nueva tarea a ingresar
  */
-Nodo * crearNodo(Tarea tarea)
+Nodo *crearNodo(Tarea tarea)
 {
-    Nodo * nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
+    Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
 
-    nuevoNodo->T.Descripcion = (char *)malloc( (strlen(tarea.Descripcion) + 1) * sizeof(char) );
+    nuevoNodo->T.Descripcion = (char *)malloc((strlen(tarea.Descripcion) + 1) * sizeof(char));
     strcpy(nuevoNodo->T.Descripcion, tarea.Descripcion);
     nuevoNodo->T.TareaID = tarea.TareaID;
     nuevoNodo->T.Duracion = tarea.Duracion;
@@ -150,11 +190,24 @@ Nodo * crearNodo(Tarea tarea)
 }
 
 /**
- * Muestra todos los nodos de una lista
+ * Elimina un nodo de una lista
  * 
+ * @param start Nodo a eliminar
+ */
+void eliminarNodo(Nodo **start)
+{
+    Nodo *temp = (*start);
+    (*start) = (*start)->Siguiente;
+    free(temp->T.Descripcion);
+    free(temp);
+}
+
+/**
+ * Muestra todos los nodos de una lista
+ *
  * @param start Lista enlazada
  */
-void mostrarLista(Nodo * start)
+void mostrarLista(Nodo *start)
 {
     printf("\n\n*** MOSTRANDO LISTA ***\n");
 
@@ -170,11 +223,56 @@ void mostrarLista(Nodo * start)
 }
 
 /**
+ * Permite al usuario seleccionar una Tarea de una lista
+ *
+ * @param start Cabecera de la lista en donde se buscará la tarea
+ * @return Datos de la Tarea seleccionada
+ */
+Tarea seleccionarTarea(Nodo **start)
+{
+    printf("\n\n*** SELECCIONANDO UNA TAREA PARA TRANSFERIR ***");
+
+    Nodo **aux = start;
+    int idSeleccionado = 0;
+    Tarea tareaSeleccionada;
+
+    printf("\n- Digite el ID de la tarea a transferir: ");
+    fflush(stdin);
+    scanf("%d", &idSeleccionado);
+
+    // Recorro las tareas hasta encontrar la seleccionada o llegar al final de la lista
+    while ((*aux != NULL) && ((*aux)->T.TareaID != idSeleccionado))
+    {
+        aux = &(*aux)->Siguiente;
+    }
+
+    // Si aux NO es null, quiere decir que encontré una tarea con el ID indicado
+    if ((*aux) != NULL)
+    {
+        // Almaceno los datos de la tarea seleccionada en una nueva Tarea
+        tareaSeleccionada.TareaID = (*aux)->T.TareaID;
+        tareaSeleccionada.Descripcion = (char *)malloc((strlen((*aux)->T.Descripcion) + 1) * sizeof(char));
+        strcpy(tareaSeleccionada.Descripcion, (*aux)->T.Descripcion);
+        tareaSeleccionada.Duracion = (*aux)->T.Duracion;
+
+        // Elimino el nodo correspondiente a la tarea que será transferida
+        eliminarNodo(aux);
+    }
+    else
+    {
+        printf("\n\n[!] El ID ingresado no corresponde a ninguna tarea [!]\n\n");
+        tareaSeleccionada.TareaID = -1;
+    }
+
+    return tareaSeleccionada;
+}
+
+/**
  * Libera la memoria dinámica asignada a los nodos de la lista
- * 
+ *
  * @param start Lista a liberar
  */
-void liberarMemoria(Nodo ** start)
+void liberarMemoria(Nodo **start)
 {
     while (*start != NULL)
     {
